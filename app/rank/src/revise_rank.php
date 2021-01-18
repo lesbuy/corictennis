a75623c05af59f83f03fdb9d0c7ea5f5ec774149
lesbuy@gmail.com <kelike@pinduoduo.com>
2021-01-18 11:13:26 +0800

    75

--- app/rank/src/revise_rank.php
+++ app/rank/src/revise_rank.php
@@ -6,3 +6,60 @@ if (!defined('ROOT')) {$dir_arr = explode('/', __DIR__); define('ROOT', join('/'
 $gender = get_param($argv, 1, "atp", " atp wta ");
 $sd = get_param($argv, 1, "s", " s d ");
 
+$calced = [];
+
+$fp = fopen(join("/", [DATA, "calc", $gender, $sd, "year", "rank"]), "r");
+while ($line = trim(fgets($fp))) {
+    $pid = $arr[0];
+    $rank = $arr[2];
+    $point = $arr[5];
+    $plays = $arr[14];
+    $calced[$pid] = [
+        'rank' => $rank,
+        'point' => $point,
+        'plays' => $plays,
+    ];
+}
+fclose($fp);
+
+echo join("\t", [
+    'PID',
+    'Name',
+    'Rank',
+    'MyRank',
+    'Point',
+    'MyPoint',
+    'Plays',
+    'MyPlays',
+    'DiffRank',
+    'DiffPoint',
+    'DiffPlays',
+]) . "\n";
+
+$fp = fopen(join("/", [DATA, "rank", $gender, $sd, "current"]), "r");
+while ($line = trim(fgets($fp))) {
+    $pid = $arr[0];
+    $name = $arr[1];
+    $rank = $arr[2];
+    $point = $arr[3];
+    $plays = $arr[4];
+
+    $calcRank = $calced[$pid]['rank'];
+    $calcPoint = $calced[$pid]['point'];
+    $calcPlays = $calced[$pid]['plays'];
+
+    echo join("\t", [
+        $pid,
+        $name,
+        $rank,
+        $calcRank,
+        $point,
+        $calcPoint,
+        $plays,
+        $calcPlays,
+        $calcRank - $rank,
+        $calcPoint - $point,
+        $calcPlays - $plays,
+    ]) . "\n";
+}
+fclose($fp);
